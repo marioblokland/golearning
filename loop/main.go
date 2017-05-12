043f9c338ce69902d0edd73ab796417496c79a2b
Mario Blokland <mario@marioblokland.com>
2017-05-12 22:07:21 +0200

    For loop in go

---
+++ loop/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Decimal \t Binary \t Hexadecimal \t UTF-8")
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t\t %b \t\t %x \t\t %q \n", i, i, i, i)
+	}
+}
+
+
