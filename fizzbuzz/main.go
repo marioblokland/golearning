74a667aacc8d7191e9a990a78b3a52791f2fd0a1
Mario Blokland <mario@marioblokland.com>
2017-05-16 21:29:08 +0200

    Fizzbuzz

---
+++ fizzbuzz/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println(i, "-- FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println(i, "-- Fizz")
+		} else if i%5 == 0 {
+			fmt.Println(i, "-- Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
