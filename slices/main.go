2292d690b93d8df2cd63df85485478f449905728
Mario Blokland <mario@marioblokland.com>
2017-05-20 13:46:23 +0200

    Slices.

---
+++ slices/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// This first slice has a length of 3 and a capacity of 3
+	// If the capacity gets exceeded, a new slice will be created with
+	// the capacity of 4 and so on. This could cost performance issues at
+	// some point
+	fmt.Println("Directly made slice")
+	someSlice := []string{"Good morning", "Guten Morgen", "Goeden dag"}
+	fmt.Println(someSlice)
+	fmt.Println(len(someSlice))
+	fmt.Println(cap(someSlice))
+
+	fmt.Println(strings.Repeat("=", 40))
+
+	// This slice has a current length of 3 and a capacity of 10.
+	// We can add new entries to the slice and the capacity will still be 10.
+	// If we exceed 10, the capacity will be doubled to 20 and so on.
+	anotherSlice := make([]int, 3, 10)
+	fmt.Println("Slice made with make")
+	fmt.Println(anotherSlice)
+	fmt.Println(len(anotherSlice))
+	fmt.Println(cap(anotherSlice))
+}
