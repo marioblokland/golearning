888e145c5641d688512784e225302690af8a1bbd
Mario Blokland <mario@marioblokland.com>
2017-05-27 10:47:13 +0200

    Iota.

--- iota/main.go
+++ iota/main.go
@@ -5,17 +5,15 @@ import "fmt"
 const (
 	// iota always increments the next iota variable value by 1.
 	// So the first iota is 0, the next would be 0 + 1 and so on.
-	// In contrast to other languages, constants in go should not
-	// be written in uppercase.
 	_  = iota             // 0 and "_" says, that this variable is not used
-	kb = 1 << (iota * 10) // 1 << (1 * 10)
-	mb = 1 << (iota * 10) // 1 << (2 * 10)
-	gb = 1 << (iota * 10) // 1 << (3 * 10)
+	KB = 1 << (iota * 10) // 1 << (1 * 10)
+	MB = 1 << (iota * 10) // 1 << (2 * 10)
+	GB = 1 << (iota * 10) // 1 << (3 * 10)
 )
 
 func main() {
 	fmt.Println("Binary \t\t\t\t\t Decimal")
-	fmt.Printf("%b \t\t\t\t %d \n", kb, kb)
-	fmt.Printf("%b \t\t\t %d \n", mb, mb)
-	fmt.Printf("%b \t %d \n", gb, gb)
+	fmt.Printf("%b \t\t\t\t %d \n", KB, KB)
+	fmt.Printf("%b \t\t\t %d \n", MB, MB)
+	fmt.Printf("%b \t %d \n", GB, GB)
 }
